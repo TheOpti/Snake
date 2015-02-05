120f62f9e3120e0bd8440ecda31773b65054b0b2
Piotr Więckowski <thespyker@wp.pl>
2015-02-05 20:32:09 +0100

    First commit

---
+++ snake.js
@@ -0,0 +1,140 @@
+
+var WIDTH = 400;
+var HEIGHT = 300;
+
+var $canvas = $('#canvas');
+
+$canvas.attr('width', WIDTH);
+$canvas.attr('height', HEIGHT);
+
+var canvas = $canvas[0];
+var ctx = canvas.getContext('2d');
+
+document.onkeydown = checkKey;
+
+var snake = [];
+var food = {};
+var points = 0;
+
+direction = 'right';
+
+function initializeGame() {
+    drawBoard();
+    createSnake();
+    drawSnake(snake);
+    generateFood();  
+    gameLoop(); 
+}    
+
+function gameLoop() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(0, 0, WIDTH, HEIGHT);
+    var frameLength = 100;
+    var next = checkDirection();
+    var newPos = snake.pop();
+    newPos.x = next.X;
+    newPos.y = next.Y;
+    snake.unshift(newPos);
+    drawSnake(snake);
+    paintCell(food.x, food.y);
+    setTimeout(gameLoop, frameLength);
+}
+
+function checkDirection() {
+    if (direction === 'right') {
+        return {
+            X: snake[0].x + 1,
+            Y: snake[0].y + 0
+        };
+    } else if (direction === 'left') {
+        return {
+            X: snake[0].x - 1,
+            Y: snake[0].y + 0
+        };
+    } else if (direction === 'up') {
+        return {
+            X: snake[0].x + 0,
+            Y: snake[0].y + 1
+        };
+    } else if (direction === 'down') {
+        return {
+            X: snake[0].x + 0,
+            Y: snake[0].y - 1
+        };
+    } else {
+        return {
+            X: snake[0].x + 0,
+            Y: snake[0].y + 0
+        };
+    }
+}
+
+function checkKey(e) {
+    e = e || window.event;
+
+    if (e.keyCode === 38 && direction !== 'up') {
+        direction = 'down';
+    }
+    else if (e.keyCode === 40 && direction !== 'down') {
+        direction = 'up';
+    }
+    else if (e.keyCode === 37 && direction !== 'right') {
+       direction = 'left';
+    }
+    else if (e.keyCode === 39 && direction !== 'left') {
+       direction = 'right';
+    }
+
+}
+
+function drawSnake(snake) {
+    for (var i = 0; i< snake.length; i++) {
+        var c = snake[i];
+        paintCell(c.x, c.y);
+    }
+}
+
+function generateFood() {
+    food = {
+        x: Math.round(Math.random() * (WIDTH - 10) / 10),
+        y: Math.round(Math.random() * (HEIGHT - 10) / 10),
+    };
+    if (!checkIfFoodXYCorrect()) {
+        generateFood();
+    }
+}
+
+function checkIfFoodXYCorrect() {
+    for (var i = 0; i < snake.length; i++) {
+        if (snake[i].x === food.x && snake[i].y === food.y) {
+            return false;
+        };
+    }
+    return true;
+}
+
+function createSnake() {
+    var length = 15;
+    for (var i = length; i > 10 ; i--) {
+        snake.push({x:i, y:5});
+    }
+    var i = 5;
+};
+
+function paintCell(x, y) {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(x*10, y*10, 10, 10);
+    ctx.strokeStyle = "white";
+    ctx.strokeRect(x*10, y*10, 10, 10);
+}
+
+
+function drawBoard() {
+    ctx.fillStyle = '#fe57a1'; //hot pink!
+    $canvas.css("border", "1px solid #000000");
+}
+
+
+initializeGame();
